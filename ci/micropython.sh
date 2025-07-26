9f291d828a110af5491933c1f78ac713f58d264f
Kenny Root <kenny@the-b.org>
2025-07-26 14:43:53 -0700

    Also install cmake

--- ci/micropython.sh
+++ ci/micropython.sh
@@ -62,7 +62,7 @@ function ci_micropython_build_mpy_cross {
 }
 
 function ci_apt_install_build_deps {
-    sudo apt-get update && sudo apt-get install --no-install-recommends --yes ccache
+    sudo apt-get update && sudo apt-get install --no-install-recommends --yes ccache cmake
 }
 
 function ci_prepare_all {
