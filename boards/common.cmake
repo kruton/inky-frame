fb57588aa58cd1543e88e1dee1229e75c56ce3ad
Kenny Root <kenny@the-b.org>
2025-07-26 17:12:49 -0700

    Use Python3 in CMake explicitly

--- boards/common.cmake
+++ boards/common.cmake
@@ -1,13 +1,13 @@
 # Make sure we get our VirtualEnv Python
-set(Python_FIND_VIRTUALENV "FIRST")
-set(Python_FIND_UNVERSIONED_NAMES "FIRST")
-set(Python_FIND_STRATEGY "LOCATION")
-find_package (Python COMPONENTS Interpreter Development)
-if (NOT Python_FOUND)
+set(Python3_FIND_VIRTUALENV "FIRST")
+set(Python3_FIND_UNVERSIONED_NAMES "FIRST")
+set(Python3_FIND_STRATEGY "LOCATION")
+find_package (Python3 COMPONENTS Interpreter Development)
+if (NOT Python3_FOUND)
     message(FATAL_ERROR "Python not found. Please ensure it is installed and its path is added to CMAKE_PREFIX_PATH.")
 endif()
 
-message("dir2uf2/py_decl: Using Python ${Python_EXECUTABLE}")
+message("dir2uf2/py_decl: Using Python ${Python3_EXECUTABLE}")
 MESSAGE("dir2uf2/py_decl: Using pimoroni tools dir ${PIMORONI_TOOLS_DIR}")
 
 # Convert supplies paths to absolute, for a quieter life
@@ -17,7 +17,7 @@ get_filename_component(PIMORONI_UF2_DIR ${PIMORONI_UF2_DIR} REALPATH)
 if (EXISTS "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py")
     MESSAGE("py_decl: py_decl.py found, will verify uf2.")
     add_custom_target("${MICROPY_TARGET}-verify" ALL
-        COMMAND ${Python_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py" --to-json --verify "${CMAKE_CURRENT_BINARY_DIR}/${MICROPY_TARGET}.uf2"
+        COMMAND ${Python3_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py" --to-json --verify "${CMAKE_CURRENT_BINARY_DIR}/${MICROPY_TARGET}.uf2"
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         COMMENT "pydecl: Verifying ${MICROPY_TARGET}.uf2"
         DEPENDS ${MICROPY_TARGET}
@@ -28,7 +28,7 @@ if (EXISTS "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" AND EXISTS "${PIMORONI_UF2_MA
     MESSAGE("dir2uf2: Using manifest ${PIMORONI_UF2_MANIFEST}.")
     MESSAGE("dir2uf2: Using root ${PIMORONI_UF2_DIR}.")
     add_custom_target("${MICROPY_TARGET}-with-filesystem.uf2" ALL
-        COMMAND ${Python_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" --fs-compact --sparse --append-to "${MICROPY_TARGET}.uf2" --manifest "${PIMORONI_UF2_MANIFEST}" --filename with-filesystem.uf2 "${PIMORONI_UF2_DIR}"
+        COMMAND ${Python3_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" --fs-compact --sparse --append-to "${MICROPY_TARGET}.uf2" --manifest "${PIMORONI_UF2_MANIFEST}" --filename with-filesystem.uf2 "${PIMORONI_UF2_DIR}"
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         COMMENT "dir2uf2: Appending filesystem to ${MICROPY_TARGET}.uf2."
         DEPENDS ${MICROPY_TARGET}
